1992f9b9ff604ecf1ac02e4e60ed154ee92a1869
s3r-b6 <103552324+s3r-b6@users.noreply.github.com>
2022-07-08 00:19:55 +0200

    base for the game logic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+function Header() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <header className="App-header">
+      <h1> Wordle React App </h1>
+    </header>
+  );
+}
+
+function Board() {
+  return (
+    <>
+      <Tile />
+      <Tile />
+      <Tile />
+      <Tile />
+      <Tile />
+      <Tile />
+    </>
+  );
+}
+
+function Tile() {
+  return (
+    <div className="tile">
+      <div className="grid"></div>
+      <div className="grid"></div>
+      <div className="grid"></div>
+      <div className="grid"></div>
+      <div className="grid"></div>
     </div>
+  );
+}
+
+function App() {
+  const [currentWord, setCurrentWord] = useState('');
+
+  useEffect(() => {
+    async function getNewWord() {
+      let wordList = await fetch(
+        'https://random-word-api.herokuapp.com/word?length=5'
+      );
+      wordList = await wordList.json();
+      let randomWord = await wordList[
+        Math.floor(Math.random() * wordList.length)
+      ];
+      setCurrentWord(randomWord);
+    }
+    getNewWord();
+    window.addEventListener('keypress', (e) => {
+      handleKeyPress(e);
+    });
+  }, []);
+
+  function handleKeyPress(e) {
+    console.log(e.key);
+  }
+
+  return (
+    <>
+      <Header />
+      <div className="App">
+        {currentWord}
+        <Board />
+      </div>
+    </>
   );
 }
 
