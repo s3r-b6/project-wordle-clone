33f0155150a57050deccdf1690edda4f0ed3c425
s3r-b6 <103552324+s3r-b6@users.noreply.github.com>
2022-07-08 18:17:01 +0200

    Progress
    Having problems making tiles to update on keypress after a tile is fullfilled, but a lot of progress has been made. Switched to 2D array so there is no need to refactor how data is stored and drawn.

--- src/App.js
+++ src/App.js
@@ -1,43 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 
-function Header() {
-  return (
-    <header className="App-header">
-      <h1> Wordle React App </h1>
-    </header>
-  );
-}
-
-function Board() {
-  return (
-    <>
-      <Tile />
-      <Tile />
-      <Tile />
-      <Tile />
-      <Tile />
-      <Tile />
-    </>
-  );
-}
-
-function Tile() {
-  return (
-    <div className="tile">
-      <div className="grid"></div>
-      <div className="grid"></div>
-      <div className="grid"></div>
-      <div className="grid"></div>
-      <div className="grid"></div>
-    </div>
-  );
-}
-
 function App() {
   const [currentWord, setCurrentWord] = useState('');
+  //2D array to handle lines
+  const [newInput, setNewInput] = useState([
+    Array(5),
+    Array(5),
+    Array(5),
+    Array(5),
+    Array(5),
+    Array(5),
+  ]);
+  const [enabledTile, setEnabledTile] = useState(0);
+  const [isGameOver, setGameOver] = useState(false);
 
   useEffect(() => {
+    getNewWord();
+
     async function getNewWord() {
       let wordList = await fetch(
         'https://random-word-api.herokuapp.com/word?length=5'
@@ -48,14 +28,82 @@ function App() {
       ];
       setCurrentWord(randomWord);
     }
-    getNewWord();
+
     window.addEventListener('keypress', (e) => {
       handleKeyPress(e);
     });
   }, []);
 
+  function Header() {
+    return (
+      <header className="App-header">
+        <h1> Wordle React App </h1>
+      </header>
+    );
+  }
+
+  function Board() {
+    function Tile(props) {
+      return (
+        <div className={`tile ${props.number}`}>
+          <div className="grid">{newInput[parseInt(props.number)][0]}</div>
+          <div className="grid">{newInput[parseInt(props.number)][1]}</div>
+          <div className="grid">{newInput[parseInt(props.number)][2]}</div>
+          <div className="grid">{newInput[parseInt(props.number)][3]}</div>
+          <div className="grid">{newInput[parseInt(props.number)][4]}</div>
+        </div>
+      );
+    }
+
+    return (
+      <>
+        <Tile number="0" />
+        <Tile number="1" />
+        <Tile number="2" />
+        <Tile number="3" />
+        <Tile number="4" />
+        <Tile number="5" />
+      </>
+    );
+  }
+
   function handleKeyPress(e) {
-    console.log(e.key);
+    const isUndefined = (el) => el === undefined;
+
+    if (e.key === 'Enter') {
+      if (newInput[enabledTile].findIndex(isUndefined) === -1) checkWord();
+      else return;
+
+      function checkWord() {
+        console.log('test');
+        if (enabledTile === 5) {
+          setGameOver(true);
+        } else {
+          let newEnabled = enabledTile + 1;
+          setEnabledTile(newEnabled);
+          console.log(newEnabled);
+          return;
+        }
+      }
+    }
+
+    //if there are no empty spaces, don't continue
+    if (newInput[enabledTile].findIndex(isUndefined) === -1) return;
+    else if (/[A-Za-z]/.test(e.key)) {
+      console.log(e.key);
+      let handledInputItem = newInput[enabledTile];
+      console.log(handledInputItem);
+      let newInputIndex = handledInputItem.findIndex(isUndefined);
+      handledInputItem[newInputIndex] = e.key;
+      console.log(handledInputItem);
+
+      let handledInput = newInput;
+      handledInput[enabledTile] = handledInputItem;
+      console.log(handledInput);
+      //the slice is to let react know that state has changed, has no real effect.
+      setNewInput(handledInput.slice());
+      console.log(newInput);
+    }
   }
 
   return (
